39540bc8d3c9a3639597611723e9032a7433a505
Ribin <ribinroy4@gmail.com>
2021-04-28 03:18:08 +0530

    Nav bar done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.scss';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NavPanel from './component/NavPanel';
 
 function App() {
     return (
         <>
-            <NavPanel />
             <Router>
+                <NavPanel />
                 <Switch>
                     <Route path='/' exact>
                         <Home />
--- src/component/NavPanel/index.jsx
+++ src/component/NavPanel/index.jsx
@@ -1,6 +1,18 @@
 import './styles.scss';
 import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as BigLogo } from './../../assets/svg/Logo Full.svg';
+import { ReactComponent as OverViewIcon } from './../../assets/svg/panelIcons/icon_overview.svg';
+import { ReactComponent as PlusIcon } from './../../assets/svg/panelIcons/icon_plus.svg';
+import { ReactComponent as OTC } from './../../assets/svg/panelIcons/icon_checked doc.svg';
+import { ReactComponent as PassivePool } from './../../assets/svg/panelIcons/icon_clock.svg';
+import { ReactComponent as NyanDocum } from './../../assets/svg/panelIcons/icon_documentation.svg';
+import { ReactComponent as Rewards } from './../../assets/svg/panelIcons/icon_dollar.svg';
+import { ReactComponent as ActivePools } from './../../assets/svg/panelIcons/icon_earnings.svg';
+import { ReactComponent as Yield } from './../../assets/svg/panelIcons/icon_flower.svg';
+import { ReactComponent as GitHub } from './../../assets/svg/panelIcons/icon_github.svg';
+import { ReactComponent as Swap } from './../../assets/svg/panelIcons/icon_swap.svg';
+import { ReactComponent as Community } from './../../assets/svg/panelIcons/icon_telegram.svg';
 
 const Test = () => {
     const [expanded, setExpand] = useState(false);
@@ -14,18 +26,105 @@ const Test = () => {
             className={expanded ? ' expanded' : ''}
             onMouseEnter={() => setExpand(true)}
             onMouseLeave={() => setExpand(false)}>
-            <BigLogo className='nyan_logo' />
+            <Link to='/'>
+                <BigLogo className='nyan_logo' />
+            </Link>
 
             <div className='header_items'>
-                <input
-                    id='s2'
-                    type='checkbox'
-                    class='switch'
-                    onChange={nightModeHandler}
-                />
-                <label for='s2'>Night Mode</label>
+                <div className='connection only_onExpanded'>
+                    <input
+                        type='text'
+                        name='connectId'
+                        id='connectId'
+                        value='Ox1234567890'
+                    />
+                    <button>Disconnect</button>
+                </div>
+                <div className='nav_links_set_wrap'>
+                    <div className='head only_onExpanded'>DASHBOARDS</div>
+                    <NavLink
+                        link='fund-overview'
+                        name='Fund Overview'
+                        Icon={OverViewIcon}
+                    />
+                    <NavLink
+                        link='create-a-pool'
+                        name='Create a pool'
+                        Icon={PlusIcon}
+                    />
+                    <NavLink
+                        link='active-pools'
+                        name='Active Pools'
+                        Icon={ActivePools}
+                    />
+                    <NavLink
+                        link='passive-pools'
+                        name='Passive Pools'
+                        Icon={PassivePool}
+                    />
+                    <NavLink
+                        link='nyan-rewards'
+                        name='Nyan Rewards'
+                        Icon={Rewards}
+                    />
+                </div>
+                <div className='nav_links_set_wrap'>
+                    <div className='head only_onExpanded'>
+                        CONTRACT INTERFACES
+                    </div>
+                    <NavLink link='swap' name='Swap' Icon={Swap} />
+                    <NavLink link='yield' name='Yield' Icon={Yield} />
+                    <NavLink link='otc-swap' name='OTC Swap' Icon={OTC} />
+                </div>
+                <div className='nav_links_set_wrap'>
+                    <div className='special_link'>
+                        <NyanDocum />
+                        <div className='text_wrap only_onExpanded'>
+                            <div className='main'>Nyan Documentation</div>
+                        </div>
+                    </div>
+                    <div className='special_link'>
+                        <Community />
+                        <div className='text_wrap only_onExpanded'>
+                            <div className='main'>Join Community</div>
+                            <div className='descr'>
+                                Join our awesome Telegram
+                            </div>
+                        </div>
+                    </div>
+                    <div className='special_link'>
+                        <GitHub />
+                        <div className='text_wrap only_onExpanded'>
+                            <div className='main'>Github</div>
+                            <div className='descr'>View source code</div>
+                        </div>
+                    </div>
+                </div>
+                <div className='theme_shift only_onExpanded'>
+                    <input
+                        id='s2'
+                        type='checkbox'
+                        class='switch'
+                        onChange={nightModeHandler}
+                    />
+                    <label for='s2'>Night Mode</label>
+                </div>
             </div>
         </header>
+    );
+};
+
+const NavLink = ({ link, name, Icon }) => {
+    const location = useLocation();
+    return (
+        <Link
+            to={link}
+            className={
+                'nav_link' + (location.pathname === '/' + link ? ' active' : '')
+            }>
+            <Icon />
+            <span className='only_onExpanded'>{name}</span>
+        </Link>
     );
 };
 
